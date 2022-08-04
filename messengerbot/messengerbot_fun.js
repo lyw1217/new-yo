7e9e312c6eae26c46f85aa3443282e933ad4a2ee
lyw1217 <mvl100d@gmail.com>
2022-08-04 14:51:17 +0900

    학습단어 정규식 검토 추가

--- messengerbot/messengerbot_fun.js
+++ messengerbot/messengerbot_fun.js
@@ -10,9 +10,9 @@ const scriptName = "fun";
  */
 
 
-let db_root = "newyo/db/"
+let db_root = "newyo/db/";
 let comm_db = db_root + "comm/";
-let fun_db = db_root + "fun/"
+let fun_db = db_root + "fun/";
 let room_db = db_root + "room/";
 let user_db = db_root + "user/";
 let learn_db = db_root + "learn/";
@@ -25,6 +25,12 @@ let data;
 
 let words_list = getLearnedListArr();
 const MAX_LEARN_NUM = 500;
+
+function findNotPermitWords(str) {
+  const regex = /[\|\\\\\?\*\<\"\:\>\/]/g;
+
+  return regex.test(str);
+}
 
 function getDrwNo( r ){
   if ( r == 0 ) {
@@ -60,9 +66,9 @@ function getRandomIntInclusive(min, max) {
 function getLearnedListArr() {
   let db_word = DataBase.getDataBase(learn_db_list);
   if (db_word == null) {
-    return null;
+    DataBase.setDataBase(learn_db_list, "\n");
   }
-  
+
   return db_word.split('\n');
 }
 
@@ -84,10 +90,11 @@ function response(
 
   if (run == "t") {
     words_list = getLearnedListArr();
-    if ( words_list.includes(msg) ) {
+    if ( words_list.length > 0  && words_list.includes(msg) ) {
       resp += DataBase.getDataBase(learn_db + msg);
     } else {   
       try {
+
         if (msg.includes("/로또")) {
           if (msg.includes("생성")) {
             let numbers = new Set();
@@ -125,8 +132,10 @@ function response(
               resp += "로또 회차 정보를 조회하지 못했어요.";
             }
           }
-        } else if (msg.includes("/가르치기")) {
-          
+        }
+        
+
+        else if (msg.includes("/가르치기")) {
           if ( msg.includes("영우") ) {
             resp += "어허!";
           } else if ( msg != "/가르치기 " ) {
@@ -135,62 +144,78 @@ function response(
               const words = input_learn_words.split("=");
               const word1 = words[0].trim();
               const word2 = words[1].trim();
-              let f_already = false;
-              words_list = getLearnedListArr();
-              if ( words_list != null ) {
-                for (let w of words_list) {
-                  if ( w == word1 ) {
-                    f_already = true;
-                    break;
+              if ( findNotPermitWords(word1) ) {
+                resp += "가르칠 수 없는 단어가 포함되어 있어요.";
+              } else {
+                let f_already = false;
+                words_list = getLearnedListArr();
+                if ( words_list.length > 0 ) {
+                  for (let w of words_list) {
+                    if ( w == word1 ) {
+                      f_already = true;
+                      break;
+                    }
                   }
                 }
-              }
-
-              if ( !f_already ) {
-                if ( words_list.length > MAX_LEARN_NUM ) {
-                  resp += "너무 많이 배웠어요.";  
+  
+                if ( !f_already ) {
+                  if ( words_list.length > MAX_LEARN_NUM ) {
+                    resp += "너무 많이 배웠어요.";
+                  } else {
+                    DataBase.setDataBase(learn_db + word1, word2);
+                    DataBase.appendDataBase(learn_db_list, word1 + "\n");
+                    resp += word1 + "을(를) ";
+                    resp += word2 + "(으)로 학습했어요.";
+                  }
                 } else {
-                  DataBase.setDataBase(learn_db + word1, word2);
-                  DataBase.appendDataBase(learn_db_list, word1 + "\n");
-                  resp += word1 + "을(를) ";
-                  resp += word2 + "(으)로 학습했어요.";
+                  resp += word1 + "은(는) 이미 ";
+                  resp += DataBase.getDataBase(learn_db + word1) + "(으)로 학습되어 있어요.";
                 }
-              } else {
-                resp += word1 + "은(는) 이미 ";
-                resp += DataBase.getDataBase(learn_db + word1) + "(으)로 학습되어 있어요.";
               }
             }
           }
-        } else if (msg.includes("/학습제거")) {
-          if ( msg != "/학습제거 " ) {
+        }
+        
+        
+        else if (msg.includes("/학습제거")) {
+          if ( msg.substr(0, "/학습제거 ".length) == "/학습제거 " ) {
             const input_del_words = msg.substring("/학습제거 ".length).trim();
             let db_word = DataBase.getDataBase(learn_db + input_del_words);
             if ( db_word != null ) {
-              if ( DataBase.removeDataBase(learn_db + input_del_words) ) {
-                words_list = getLearnedListArr();
-                if ( words_list != null ) {
-                  let filtered_list = words_list.filter((element) => element !== input_del_words);
-                  DataBase.setDataBase(learn_db_list, filtered_list.join('\n'));
-                  resp += input_del_words + " 단어 학습 내용을 제거했어요.";
+              try {
+                if ( DataBase.removeDataBase(learn_db + input_del_words) ) {
+                  words_list = getLearnedListArr();
+                  if ( words_list.length > 0 ) {
+                    let filtered_list = words_list.filter((element) => element !== input_del_words);
+                    DataBase.setDataBase(learn_db_list, filtered_list.join('\n'));
+                    resp += input_del_words + " 단어 학습 내용을 제거했어요.";
+                  } else {
+                    resp += "제거할 " + input_del_words + "이(가) list에 존재하지 않아요.";
+                  }
                 } else {
-                  resp += "제거할 " + input_del_words + "이(가) list에 존재하지 않아요.";
-                }
-              } else {
+                  resp += input_del_words + " 단어 학습 내용을 제거하는데 실패했어요.";
+                }  
+              } catch (error) {
                 resp += input_del_words + " 단어 학습 내용을 제거하는데 실패했어요.";
               }
             } else {
               resp += input_del_words + " 단어는 학습하지 않았어요.";
             }
           }
-        } else if (msg.includes("/학습리스트") && sender.includes("master")) {
+
+        }
+        
+        
+        else if (msg.includes("/학습리스트") && sender.includes("master")) {
           if ( msg.includes("제거") ) {
             const del_count = parseInt(msg.substring("/학습리스트 제거 ".length).trim());
             words_list = getLearnedListArr();
-            if ( words_list != null ) {
+            if ( words_list.length > 0 ) {
               words_list.splice(0, del_count);
               DataBase.setDataBase(learn_db_list, words_list.join('\n'));
               resp += "오래된 " + del_count.toString() + "개 단어들의 학습 내용을 제거했어요.";
             }
+
           } else {
             words_list = getLearnedListArr();
             if ( words_list.length > 1) {
@@ -203,6 +228,7 @@ function response(
             }
           } 
         }
+        
       } catch (error) {
         resp += "에러 발생.\n err : " + error;
       }
